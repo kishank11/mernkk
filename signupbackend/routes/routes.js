db55cb9f0db13eee0ea1e318f0a91b70da1a7aec
kishank11 <kishankanojia11@gmail.com>
2021-10-09 22:47:39 +0530

    first commit

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -2,11 +2,9 @@ const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy= require('../models/SignUpModel')
 const bcrypt = require('bcrypt')
-const app= express();
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static("../../build"));
-}
+
+
  router.post('/signup',async (request,response)=>{
 
     const saltPassword = await bcrypt.genSalt(10)
